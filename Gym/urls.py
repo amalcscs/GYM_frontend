7807aed99297cb08ed4cb15aca396d2813083087
amal c s <amalcs0006@gmail.com>
2022-04-14 18:01:23 +0530

    third changes

--- Gym/urls.py
+++ Gym/urls.py
@@ -10,7 +10,115 @@ from app import views
 urlpatterns = [
 
     re_path('admin/', admin.site.urls),
-     re_path(r'^$', views.Main_page, name='Main_page'),
+    re_path(r'^$', views.Main_page, name='Main_page'),
+
+    ########### Anandhu Dashboard Section ##########
+           
+    re_path(r'^SuperAdminGYM_index$', views.SuperAdminGYM_index, name='SuperAdminGYM_index'),
+    re_path(r'^SuperAdminGYM_Dashboard$', views.SuperAdminGYM_Dashboard, name='SuperAdminGYM_Dashboard'),
+    re_path(r'^SuperAdminGYM_Total_Instructors$', views.SuperAdminGYM_Total_Instructors, name='SuperAdminGYM_Total_Instructors'),
+    re_path(r'^SuperAdminGYM_TotalTraineesUPhysicalTrainer$', views.SuperAdminGYM_TotalTraineesUPhysicalTrainer, name='SuperAdminGYM_TotalTraineesUPhysicalTrainer'),
+    re_path(r'^SuperAdminGYM_TotalTraineesUPhysicalTrainer2$', views.SuperAdminGYM_TotalTraineesUPhysicalTrainer2, name='SuperAdminGYM_TotalTraineesUPhysicalTrainer2'),
+    re_path(r'^SuperAdminGYM_TraineeProfile$', views.SuperAdminGYM_TraineeProfile, name='SuperAdminGYM_TraineeProfile'),
+    re_path(r'^SuperAdminGYM_TraineeProfile1$', views.SuperAdminGYM_TraineeProfile1, name='SuperAdminGYM_TraineeProfile1'),
+    
+        ########### Anandhu Dashboard Section ##########
+
+        ########### Anandhu Batch Section ##########  
+
+    re_path(r'^SuperAdminGYM_Batch$', views.SuperAdminGYM_Batch, name='SuperAdminGYM_Batch'),
+
+        ########### Anandhu Batch Section ends ##########
+
+        ############# Praveen Dashboard Section ##############
+    
+    re_path(r'^SuperAdmin_trainees/$', views.SuperAdmin_trainees, name='SuperAdmin_trainees'),
+    re_path(r'^SuperAdmin_ActiveTrainees/$', views.SuperAdmin_ActiveTrainees, name='SuperAdmin_ActiveTrainees'),
+    re_path(r'^SuperAdmin_PassiveTrainees/$', views.SuperAdmin_PassiveTrainees, name='SuperAdmin_PassiveTrainees'),
+    re_path(r'^SuperAdmin_ActiveTraineeProfile/$', views.SuperAdmin_ActiveTraineeProfile, name='SuperAdmin_ActiveTraineeProfile'),
+    re_path(r'^SuperAdmin_PassiveTraineeProfile/$', views.SuperAdmin_PassiveTraineeProfile, name='SuperAdmin_PassiveTraineeProfile'),
+    re_path(r'^SuperAdmin_Machines/$', views.SuperAdmin_Machines, name='SuperAdmin_Machines'),
+    
+        ############# Praveen Dashboard Section ends ##############
+
+        ############# Nimisha Expense Section ##############
+
+    re_path(r'^SuperAdmin_Expense$',views.SuperAdmin_Expense,name='SuperAdmin_Expense'),
+    re_path(r'^SuperAdmin_ExpenseView$',views.SuperAdmin_ExpenseView,name='SuperAdmin_ExpenseView'),
+    re_path(r'^SuperAdmin_NewTransaction$',views.SuperAdmin_NewTransaction,name='SuperAdmin_NewTransaction'),
+    re_path(r'^SuperAdmin_ExpenseViewEdit$',views.SuperAdmin_ExpenseViewEdit,name='SuperAdmin_ExpenseViewEdit'),
+    re_path(r'^SuperAdmin_FindExpense$',views.SuperAdmin_FindExpense,name='SuperAdmin_FindExpense'),
+    re_path(r'^SuperAdmin_FindExpenseView$',views.SuperAdmin_FindExpenseView,name='SuperAdmin_FindExpenseView'),
+    re_path(r'^SuperAdmin_FindExpenseViewEdit$',views.SuperAdmin_FindExpenseViewEdit,name='SuperAdmin_FindExpenseViewEdit'),
+    re_path(r'^SuperAdmin_FindExpenseNewTransaction$',views.SuperAdmin_FindExpenseNewTransaction,name='SuperAdmin_FindExpenseNewTransaction'),
+
+        ############# Nimisha Expense Section ends ############## 
+
+        ############# Sanjay Machine Section   ##################
+    re_path(r'^gymmachine$', views.gymmachine, name='gymmachine'),
+    re_path(r'^addcategory$', views.addcategory, name='addcategory'),
+    re_path(r'^viewcategory$', views.viewcategory, name='viewcategory'),
+    re_path(r'^viewmachines$', views.viewmachines, name='viewmachines'),
+    re_path(r'^chestpressmachine$', views.chestpressmachine, name='chestpressmachine'),
+    re_path(r'^dumbbell$', views.dumbbell, name='dumbbell'),
+    re_path(r'^preacher$', views.preacher, name='preacher'),
+    re_path(r'^legpress$', views.legpress, name='legpress'),
+    re_path(r'^form$', views.form, name='form'),
+    re_path(r'^chestview$', views.chestview, name='chestview'),
+    re_path(r'^shoulderview$', views.shoulderview, name='shoulderview'),
+    re_path(r'^triceps$', views.triceps, name='triceps'),
+    re_path(r'^bicepsview$', views.bicepsview, name='bicepsview'),
+
+    ###############  Sanjay Machine Section Ends #################
+
+    ############### Unnikrishnan workout section #################
+    re_path(r'^superadmin_workout$',views.superadmin_workout,name='superadmin_workout'),
+    re_path(r'^superadmin_viewworkout$',views.superadmin_viewworkout,name='superadmin_viewworkout'),
+    re_path(r'^superadmin_addworkout$',views.superadmin_addworkout,name='superadmin_addworkout'),
+    re_path(r'^superadmin_ChestDetailView$',views.superadmin_ChestDetailView,name='superadmin_ChestDetailView'),
+    re_path(r'^superadmin_BackDetailView$',views.superadmin_BackDetailView,name='superadmin_BackDetailView'),
+
+    ############### Unnikrishnan workout section ends #################
+
+############# subeesh payment Section ##############
+
+    re_path(r'^SuperAdmin_pay_det/$', views.SuperAdmin_pay_det, name='SuperAdmin_pay_det'),
+    re_path(r'^SuperAdmin_current_trainees/$', views.SuperAdmin_current_trainees, name='SuperAdmin_current_trainees'),
+    re_path(r'^SuperAdmin_current_trainees_payment/$', views.SuperAdmin_current_trainees_payment, name='SuperAdmin_current_trainees_payment'),
+    re_path(r'^SuperAdmin_current_trainees_payment_add/$', views.SuperAdmin_current_trainees_payment_add, name='SuperAdmin_current_trainees_payment_add'),
+    re_path(r'^SuperAdmin_current_trainees_payment_update/$', views.SuperAdmin_current_trainees_payment_update, name='SuperAdmin_current_trainees_payment_update'),
+    re_path(r'^SuperAdmin_previous_trainees_payment/$', views.SuperAdmin_previous_trainees_payment, name='SuperAdmin_previous_trainees_payment'),
+
+
+    ############# subeesh payment Section ends ##############
+
+    ############# Akhil Physical Trainer Section ##############
+
+    re_path(r'^SuperAdmin_personal_trainer/$', views.SuperAdmin_personal_trainer, name='SuperAdmin_personal_trainer'),
+    re_path(r'^SuperAdmin_active_trainers/$', views.SuperAdmin_active_trainers, name='SuperAdmin_active_trainers'),
+    re_path(r'^SuperAdmin_resigned_trainers/$', views.SuperAdmin_resigned_trainers, name='SuperAdmin_resigned_trainers'),
+    re_path(r'^SuperAdmin_activetrainer_update/$', views.SuperAdmin_activetrainer_update, name='SuperAdmin_activetrainer_update'),
+    re_path(r'^SuperAdmin_resignedtrainer_update/$', views.SuperAdmin_resignedtrainer_update, name='SuperAdmin_resignedtrainer_update'),
+
+
+
+
+    ############# Akhil Physical Trainer Section ends ##############
+
+
+    ############# Anwar Rgistration Section ##############
+
+    re_path(r'^SuperAdmin_RegistrationDetails/$', views.SuperAdmin_RegistrationDetails, name='SuperAdmin_RegistrationDetails'),
+    re_path(r'^SuperAdmin_Activereg/$', views.SuperAdmin_Activereg, name='SuperAdmin_Activereg'),
+    re_path(r'^SuperAdmin_Updatereg/$', views.SuperAdmin_Updatereg, name='SuperAdmin_Updatereg'),
+    re_path(r'^SuperAdmin_Passivereg/$', views.SuperAdmin_Passivereg, name='SuperAdmin_Passivereg'),
+    re_path(r'^SuperAdmin_PassiveUpdate/$', views.SuperAdmin_PassiveUpdate, name='SuperAdmin_PassiveUpdate'),
+
+
+    ############# Anwar Rgistration Section ends ##############
+
+
+
     ##############################  Trainer module ###############################
 
     re_path(r'^Trainer_index$', views.Trainer_index, name='Trainer_index'),
@@ -43,7 +151,13 @@ urlpatterns = [
 
     
     
-    
+    #*********************meenu*********************
+    re_path(r'^workout$',views.workout,name='workout'),
+    re_path(r'^workoutdetails$',views.workoutdetails,name='workoutdetails'),
+    re_path(r'^workoutdetails2$',views.workoutdetails2,name='workoutdetails2'),
+    re_path(r'^trainerworkout$',views.trainerworkout,name='trainerworkout'),
+    re_path(r'^trainerworkoutdetails$',views.trainerworkoutdetails,name='trainerworkoutdetails'),
+    re_path(r'^trainerworkoutdetails2$',views.trainerworkoutdetails2,name='trainerworkoutdetails2')
     
 
 
--- app/views.py
+++ app/views.py
@@ -2,8 +2,238 @@
 from django.shortcuts import render, redirect
 
 from django.conf import settings
+
 def  Main_page(request):
     return render(request, 'Main_page.html')
+
+
+############# Anandhu Dashboard Section ##############
+
+def SuperAdminGYM_index(request):
+    return render(request,'SuperAdminGYM_index.html')
+
+def SuperAdminGYM_Dashboard(request):
+    return render(request,'SuperAdminGYM_Dashboard.html')    
+
+def SuperAdminGYM_Total_Instructors(request):
+    return render(request,'SuperAdminGYM_Total_Instructors.html')    
+
+def SuperAdminGYM_TotalTraineesUPhysicalTrainer(request):
+    return render(request,'SuperAdminGYM_TotalTraineesUPhysicalTrainer.html')    
+
+def SuperAdminGYM_TotalTraineesUPhysicalTrainer2(request):
+    return render(request,'SuperAdminGYM_TotalTraineesUPhysicalTrainer2.html')
+
+def SuperAdminGYM_TraineeProfile(request):
+    return render(request,'SuperAdminGYM_TraineeProfile.html')  
+
+def SuperAdminGYM_TraineeProfile1(request):
+    return render(request,'SuperAdminGYM_TraineeProfile1.html')  
+
+############# Anandhu Dashboard Section ends ############## 
+
+############# Anandhu Batch Section  ############## 
+
+def SuperAdminGYM_Batch(request):
+    return render(request,'SuperAdminGYM_Batch.html')     
+
+############# Anandhu Batch Section ends ############## 
+
+############# Praveen Dashboard Section ##############
+
+def SuperAdmin_trainees(request):
+    return render(request,'SuperAdmin_trainees.html')
+
+def SuperAdmin_ActiveTrainees(request):
+    return render(request,'SuperAdmin_ActiveTrainees.html')
+
+def SuperAdmin_PassiveTrainees(request):
+    return render(request,'SuperAdmin_PassiveTrainees.html')
+
+def SuperAdmin_ActiveTraineeProfile(request):
+    return render(request,'SuperAdmin_ActiveTrainerProfile.html')
+
+def SuperAdmin_PassiveTraineeProfile(request):
+    return render(request,'SuperAdmin_PassiveTrainerProfile.html')
+
+def SuperAdmin_Machines(request):
+    return render(request,'SuperAdmin_Machines.html')
+
+############# Praveen Dashboard Section ends ##############
+   
+############# Nimisha Expense Section ##############
+
+
+def SuperAdmin_Expense(request):
+    return render(request,'SuperAdmin_Expense.html')
+
+def SuperAdmin_ExpenseView(request):
+    return render(request,'SuperAdmin_ExpenseView.html')
+
+def SuperAdmin_NewTransaction(request):
+    return render(request,'SuperAdmin_NewTransaction.html')
+
+def SuperAdmin_ExpenseViewEdit(request):
+    return render(request,'SuperAdmin_ExpenseViewEdit.html')
+
+def SuperAdmin_FindExpense(request):
+    return render(request,'SuperAdmin_FindExpense.html')
+
+def SuperAdmin_FindExpenseView(request):
+    return render(request,'SuperAdmin_FindExpenseView.html')
+
+def SuperAdmin_FindExpenseViewEdit(request):
+    return render(request,'SuperAdmin_FindExpenseViewEdit.html')
+
+def SuperAdmin_FindExpenseNewTransaction(request):
+    return render(request,'SuperAdmin_FindExpenseNewTransaction.html')
+
+############# Nimisha Expense Section ends ############## 
+
+
+############ Sanjay Machine Section #####################
+
+
+def gymmachine(request):
+    return render(request,'gymmachine.html')    
+
+
+def addcategory(request):
+    return render(request,'addcategory.html') 
+
+
+def viewcategory(request):
+    return render(request,'viewcategory.html')    
+
+
+def viewmachines(request):
+    return render(request,'viewmachines.html')     
+
+def chestpressmachine(request):
+    return render(request,'chestpressmachine.html')
+
+def dumbbell(request):
+    return render(request,'dumbbell.html')
+
+
+def preacher(request):
+    return render(request,'preacher.html')    
+
+
+def legpress(request):
+    return render(request,'legpress.html')     
+
+
+def form(request):
+    return render(request,'form.html')
+
+
+def chestview(request):
+    return render(request,'chestview.html')
+
+
+def shoulderview(request):
+    return render(request,'shoulderview.html')
+
+def triceps(request):
+    return render(request,'triceps.html')
+
+def bicepsview(request):
+    return render(request,'bicepsview.html')
+
+
+########### Sanjay Machine Section End ###########
+
+############ Unnikrishnan Workout Section #####################
+
+
+def superadmin_workout(request):
+    return render(request,'superadmin_workout.html')
+
+def superadmin_viewworkout(request):
+    return render(request,'superadmin_viewworkout.html')
+
+def superadmin_addworkout(request):
+    return render(request,'superadmin_addworkout.html')
+
+def superadmin_ChestDetailView(request):
+    return render(request,'superadmin_ChestDetailView.html')
+
+def superadmin_BackDetailView(request):
+    return render(request,'superadmin_BackDetailView.html')
+
+############## Unnikrishnan workout section end ################
+
+
+############# Subeesh Payment Section ##############
+
+
+
+def SuperAdmin_pay_det(request):
+     return render(request, 'SuperAdmin_pay_det.html')
+
+def SuperAdmin_current_trainees(request):
+     return render(request, 'SuperAdmin_current_trainees.html')
+
+def SuperAdmin_current_trainees_payment(request):
+     return render(request, 'SuperAdmin_current_trainees_payment.html')
+
+def SuperAdmin_current_trainees_payment_add(request):
+     return render(request, 'SuperAdmin_current_trainees_payment_add.html')
+
+def SuperAdmin_current_trainees_payment_update(request):
+     return render(request, 'SuperAdmin_current_trainees_payment_update.html')
+
+def SuperAdmin_previous_trainees_payment(request):
+     return render(request, 'SuperAdmin_previous_trainees_payment.html')
+
+
+
+
+############# Subeesh Payment Section ends ##############
+
+
+
+
+############# Akhil Physical Trainer Section ##############
+
+def SuperAdmin_personal_trainer(request):
+    return render(request,'SuperAdmin_personal_trainer.html')
+
+def SuperAdmin_active_trainers(request):
+    return render(request,'SuperAdmin_active_trainers.html')
+
+def SuperAdmin_resigned_trainers(request):
+    return render(request,'SuperAdmin_resigned_trainers.html')
+
+def SuperAdmin_activetrainer_update(request):
+    return render(request,'SuperAdmin_activetrainer_update.html')
+
+def SuperAdmin_resignedtrainer_update(request):
+    return render(request,'SuperAdmin_resignedtrainer_update.html')
+
+
+############# Akhil Physical Trainer Section ends ##############
+
+
+############# Anwar Rgistration Section ##############
+
+
+def SuperAdmin_RegistrationDetails(request):
+    return render(request,'SuperAdmin_RegistrationDetails.html')
+def SuperAdmin_Activereg(request):
+    return render(request,'SuperAdmin_Activereg.html')
+def SuperAdmin_Updatereg(request):
+    return render(request,'SuperAdmin_Updatereg.html') 
+def SuperAdmin_Passivereg(request):
+    return render(request,'SuperAdmin_Passivereg.html') 
+def SuperAdmin_PassiveUpdate(request):
+    return render(request,'SuperAdmin_PassiveUpdate.html') 
+
+
+############# Anwar Rgistration Section ends ##############
+
+
 #***************************  Trainer module *************************
 
 def Trainer_index(request):
@@ -55,3 +285,24 @@ def Trainee_Dashboard(request):
 
 def Trainee_payment_history(request):
     return render(request,'Trainee_payment_history.html')
+
+
+
+#*********************meenu**************
+def workout(request):
+    return render(request,'workout.html')
+
+def workoutdetails(request):
+    return render(request,'workoutdetails.html')
+
+def workoutdetails2(request):
+    return render(request,'workoutdetails2.html')
+
+def trainerworkout(request):
+    return render(request,'trainerworkout.html')
+
+def trainerworkoutdetails(request):
+    return render(request,'trainerworkdetails.html')
+
+def trainerworkoutdetails2(request):
+    return render(request,'trainerworkoutdetails2.html')
